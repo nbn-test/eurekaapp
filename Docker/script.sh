fb3412edd4aac498a98db924e54b46a2c6acdfea
kiraanmore <kiraan.more@hotmail.com>
2020-02-20 11:22:09 +0000

    Test

--- Docker/script.sh
+++ Docker/script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#docker login --password=admin123 --username=admin 40141c99.ngrok.io
-scp -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa_amazonlinux springBootMongo.yml ec2-user@18.191.100.182:/home/ec2-user/
-ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa_amazonlinux ec2-user@18.191.100.182 "sudo docker login --password=admin123 --username=admin 40141c99.ngrok.io"
-ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa_amazonlinux ec2-user@18.191.100.182 "touch test"
+scp -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa springBootMongo.yml ubuntu@IPADDRESS:/home/ubuntu/
+ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa ubuntu@IPADDRESS "sudo docker login --password=admin123 --username=admin 40141c99.ngrok.io"
+ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa ubuntu@IPADDRESS \"sudo kubectl apply -f /home/ubuntu/springBootMongo.yml\""
