0999c364de2057ca28ada12fef2fa958f1447675
kiraanmore <kiraan.more@hotmail.com>
2020-02-20 11:01:44 +0000

    test

--- Docker/script.sh
+++ Docker/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #docker login --password=admin123 --username=admin 40141c99.ngrok.io
-scp -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa_amazonlinux springBootMongo.yml ec2-user@${ipaddress}:/home/ec2-user/
-ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa_amazonlinux ec2-user@${ipaddress} "docker login --password=admin123 --username=admin 40141c99.ngrok.io"
-ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa_amazonlinux ec2-user@${ipaddress} "touch test"
+scp -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa_amazonlinux springBootMongo.yml ec2-user@18.191.100.182:/home/ec2-user/
+ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa_amazonlinux ec2-user@18.191.100.182 "docker login --password=admin123 --username=admin 40141c99.ngrok.io"
+ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/.ssh/id_rsa_amazonlinux ec2-user@18.191.100.182 "touch test"
